d4aa4330a98fafe2d45eb4473f148a5510cedeb7
John Mark Schofield <jms@schof.org>
2013-10-10 11:18:03 -0700

    Install virtualenvwrapper, and set directory permissions on /usr/local/bin

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,5 +15,26 @@ class virtualenv {
         source   => 'virtualenv',
         require => Exec['easy_install'],
     }
+
+    package { 'virtualenvwrapper':
+        provider => 'pip',
+        source   => 'virtualenvwrapper',
+        require => Package['virtualenv'],
+    }
+
+    file {'/usr/local':
+        owner => root,
+        group => wheel,
+        mode => 755,
+        ensure => directory,
+    }
+
+    file {'/usr/local/bin':
+        owner => root,
+        group => wheel,
+        mode => 755,
+        ensure => directory,
+        require => File['/usr/local'],
+    }
 }
 
