3bf4bfa2a37af9a6f96a858a11ccb735943ad4b2
John Mark Schofield <jms@schof.org>
2013-10-10 11:02:20 -0700

    Installing pip before we use it.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,11 @@
 #
 #   include virtualenv
 class virtualenv {
+
+  exec { 'easy_install':
+    command => "sudo easy_install pip",
+    }
+
   package { 'virtualenv':
     provider => 'pip',
     source   => 'virtualenv',
